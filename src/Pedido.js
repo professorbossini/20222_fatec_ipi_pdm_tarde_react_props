65419e86ed8de979040cabeff7fef52c202a48e4
Rodrigo Bossini <professorbossini@gmail.com>
2022-08-19 18:05:09 -0300

    apostila04: bloco 2.7.2 concluido

---
+++ src/Pedido.js
@@ -0,0 +1,18 @@
+const Pedido = ({data, icone, titulo, descricao}) => {
+  return (
+    <div className="card">
+      <div className="card-header text-muted">{data}</div>
+      <div className="card-body d-flex">
+        <div className="d-flex align-items-center">
+        <i className={`${icone}`}></i>
+        </div>
+        <div className="flex-grow-1 ms-2 border">
+          <h4 className="text-center">{titulo}</h4>
+          <p className="text-center">{descricao}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Pedido
--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Pedido from './Pedido'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 const App = () => {
   return (
     // componente principal
-    <div className='container border rounded mt-2'>
+    <div class='container border rounded mt-2'>
       <div className='row border-bottom m-2'>
         <h1 className='display-5 text-center'>Seus pedidos</h1>
       </div>
+      {/* linha para o primeiro pedido */}
+      <div className="row">
+        <div className="col-sm-8 col-md-6 m-2">
+          {/* aqui vai entrar o primeiro pedido */}
+          <Pedido 
+            data="22/04/2021" 
+            icone="fa-solid fa-hdd fa-2x" 
+            titulo="SSD" 
+            descricao="SSD Kingston A400 - SATA"/>
+        </div>
+      </div>
 
-      
+      {/* linha para o segundo pedido */}
+      <div className="row">
+        <div className="col-sm-8 col-md-6 m-2">
+          {/* aqui vai ficar o segundo pedido */}
+          <Pedido 
+            data="20/04/2021"
+            icone="fa-solid fa-book fa-2x"
+            titulo="Livro" 
+            descricao="Concrete Mathematics - Donald Knuth"
+          />
+        </div>
+      </div>
+
+      {/* linha para o terceiro pedido */}
+      <div className="row">
+        <div className="col-sm-8 col-md-6 m-2">
+          {/* aqui vai ficar o terceiro pedido */}
+          <Pedido 
+            data="21/01/2021"
+            icone="fa-solid fa-laptop fa-2x"
+            titulo="Notebook" 
+            descricao="Notebook Dell - 8Gb - i5"
+          />
+        </div>
+      </div>
+
     </div>
     
   )
